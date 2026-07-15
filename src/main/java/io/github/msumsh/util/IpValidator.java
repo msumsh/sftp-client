d6c77cbc1fda02ff28b0d3c16560b959201b5d6b
msumsh <mss.shvetsova@gmail.com>
2026-07-15 21:52:07 +0300

    added custom IPv4 validator

--- src/main/java/io/github/msumsh/util/IpValidator.java
+++ src/main/java/io/github/msumsh/util/IpValidator.java
@@ -2,6 +2,35 @@ package io.github.msumsh.util;
 
 public class IpValidator {
     public boolean validate(String ip) {
-        return false;
+        if (ip == null || ip.isEmpty()) {
+            return false;
+        }
+
+        ip = ip.trim();
+
+        String[] nums = ip.split("\\.");
+        if (nums.length != 4) {
+            return false;
+        }
+
+        for (String num : nums) {
+            if (num.isEmpty()) {
+                return false;
+            }
+
+            int n;
+
+            try {
+                n = Integer.parseInt(num);
+            } catch (NumberFormatException e) {
+                return false;
+            }
+
+            if (n < 0 || n > 255) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
